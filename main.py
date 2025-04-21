49cc925adbf56ea76bd0b6ceb174d95758aaadd8
SeisoNeko <wf10030708@gmail.com>
2025-04-21 18:27:19 +0800

    更新 .gitignore 以排除輸出檔案，修改 main.py 以使用新的模型和資料夾路徑，刪除不再使用的 best.pt，新增 colored_detect_train.py 以訓練 YOLO 模型

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ import ultralytics
 import os
 
 if __name__ == '__main__':
-    model = ultralytics.YOLO('model/best.pt')
-    data_dir = 'dataset/v4/images/test' # Path to the images wanted to inference
+    model = ultralytics.YOLO('model/color_detect.pt')
+    data_dir = 'data/labeled' # Path to the images wanted to inference
 
     output_dir = 'output'
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
     for image_file in os.listdir(data_dir):
-        if image_file.endswith('.jpg'):
+        if image_file.endswith('.png'):
             image_name = os.path.splitext(image_file)[0]
             image_path = os.path.join(data_dir, image_file)
-            results = model(image_path, save = True, project = output_dir, name='segmentation', exist_ok=True)
+            results = model(image_path, save = True, project = output_dir, name='color', exist_ok=True)
             
             
     
