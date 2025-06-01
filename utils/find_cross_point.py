da8c1b0fdc6eb33468dc48a2fc94a6944c4b1811
PuzeLee <joshua2160puze@gmail.com>
2025-06-02 01:51:46 +0800

    Add cross check at finding cross point

--- utils/find_cross_point.py
+++ utils/find_cross_point.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import os
 import time
+import math
 
 from cv2.typing import MatLike
 
@@ -32,13 +33,18 @@ def line_intersection(line1: tuple[int, int], line2: tuple[int, int]) -> tuple[i
     # return int(px), int(py)
     return px, py
 
+# 計算直線角度
+def calculate_angle(x1, y1, x2, y2) -> float:
+    angle = math.degrees(math.atan2(y2 - y1, x2 - x1))
+    return angle + 180 if angle < 0 else angle  # 統一為 0~180°
 
 def find_cross_point(image: MatLike, output_dir: str, img_name: str) -> tuple[int, int] | None:
     # check if the output path is valid
     os.makedirs(output_dir, exist_ok=True)
     os.makedirs(f"{output_dir}/cross_point", exist_ok=True)
 
-    start_time = time.time()
+    ### debug
+    # start_time = time.time()
 
     # 轉換成 BGR 格式
     image = cv2.cvtColor(image, cv2.COLOR_BGRA2BGR) 
@@ -46,15 +52,16 @@ def find_cross_point(image: MatLike, output_dir: str, img_name: str) -> tuple[in
     # 取出高度與寬度
     height, width = image.shape[:2]
 
-    # 霍夫直線轉換 TODO: 修改成改變解析度
+    # 霍夫直線轉換
     ########### Deprecated
     # if height > 1000 and width > 1000:
     #     threshold = int(max(height, width) * 0.4)
     #     minLineLength = threshold
     #     maxLineGap = int(max(height, width) * 0.05)
     # else:
+    ### 使用固定參數
     threshold = 100
-    minLineLength = 100
+    minLineLength = 200
     maxLineGap = 10
 
     # 依據解析度改變 scale
@@ -78,8 +85,7 @@ def find_cross_point(image: MatLike, output_dir: str, img_name: str) -> tuple[in
     ### debug
     # print(f"scale_y: {scale_y}, scale_x: {scale_x}")
     # cv2.imshow("scaled_img", scaled_img)
-    # cv2.waitKey(0)  # 等待按鍵，按一次才會跳下一張
-    # cv2.destroyWindow(f"{i}")  # 顯示完關掉目前這個小視窗
+    # cv2.waitKey(0)
 
     # 灰階
     gray = cv2.cvtColor(scaled_img, cv2.COLOR_BGR2GRAY)
@@ -91,12 +97,15 @@ def find_cross_point(image: MatLike, output_dir: str, img_name: str) -> tuple[in
 
     # 儲存直線座標
     line_coords = []
+    angles = []
     ### debug
     # i = 0
     for line in lines:
         x1, y1, x2, y2 = line[0]
         line_coords.append(((x1, y1), (x2, y2)))
-        
+        angle = calculate_angle(x1, y1, x2, y2)
+        angles.append(angle)
+
         ### debug
         # 複製一張乾淨的底圖來畫線（避免每次都畫在同一張上）
         # img_copy = scaled_img.copy()
@@ -109,8 +118,28 @@ def find_cross_point(image: MatLike, output_dir: str, img_name: str) -> tuple[in
         # cv2.destroyWindow(f"{i}")  # 顯示完關掉目前這個小視窗
         # i = i + 1
 
+    # 計算平均角度
+    angles_np = np.array(angles)
+    mean_angle = np.mean(angles_np)
+
     ### debug
     # print(f"有 {i} 個線")
+    # print(f"平均角度：{mean_angle:.2f} 度")
+
+    # 判斷偏離 ±45 度的線段
+    angle_diff = np.abs(angles_np - mean_angle)
+    angle_diff = np.minimum(angle_diff, 180 - angle_diff)
+
+    deviated_lines = angle_diff > 20
+    if np.any(deviated_lines):
+        # print("發現偏離平均角度超過 20 度的線段")
+        # for i, dev in enumerate(deviated_lines):
+        #     if dev:
+        #         print(f"  線段角度：{angles[i]:.2f} 度")
+        pass
+    else:
+        # print("沒有偏離平均角度超過 20 度的線段")
+        return None
 
     # 嘗試所有線段組合找交點
     points = []
@@ -141,7 +170,7 @@ def find_cross_point(image: MatLike, output_dir: str, img_name: str) -> tuple[in
     median_x = np.median(points_arr[:,0])
     median_y = np.median(points_arr[:,1])
 
-    # max_dist = 50
+    # max_dist = max(標準差平均 or 50)
     std_x = np.std(points_arr[:, 0])  # x 座標的標準差
     std_y = np.std(points_arr[:, 1])  # y 座標的標準差
     avg_std = int((std_x + std_y) / 2)
@@ -167,19 +196,7 @@ def find_cross_point(image: MatLike, output_dir: str, img_name: str) -> tuple[in
     final_y = int(np.round(mean_y))
 
     cv2.circle(image, (final_x, final_y), 2, (0, 255, 0), -1)
-    # img[final_y, final_x] = (0, 255, 0)
     cv2.putText(image, f"({final_x},{final_y})", (final_x + 20, final_y + 5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-    """
-    print("加權平均後代表座標:", (final_x, final_y))
-
-    # 顯示結果
-    img_large = cv2.resize(img, (img.shape[1]*2, img.shape[0]*2), interpolation=cv2.INTER_NEAREST)
-    cv2.imshow("Result2", img_large) # 放大兩倍
-    cv2.imshow("Result", img) # 原圖
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    """
-
     cv2.imwrite(f"{output_dir}/cross_point/{img_name}_crosspoint.png", image)
 
     ### debug
@@ -191,16 +208,16 @@ def find_cross_point(image: MatLike, output_dir: str, img_name: str) -> tuple[in
 
 if __name__ == "__main__":
     import os
-    # input_dir = './temp/crops'  # Directory containing cropped images
+    # input_dir = '../output/1140430_construction_site_photography/temp/1140430_construction_site_photography/crops'  # Directory containing cropped images
     output_dir = '../output/temp'
 
-    img_name = "PXL_20250415_094502342_cross.png"
-    img_path = "../output/labeled_cross_photos/crops/PXL_20250415_094502342_cross.png"
+    img_name = "PXL_20250415_093923144_cross"
+    img_path = "../temp/labeled_cross_photos/crops/PXL_20250415_093923144_cross.png"
     img = cv2.imread(img_path)
-    find_cross_point(img, img_name, output_dir)
+    find_cross_point(img, output_dir, img_name)
 
     # for img_file in os.listdir(input_dir):
-    #     img_name = img_file.split('.')c[0]
+    #     img_name = img_file.split('.')[0]
     #     img_path = os.path.join(input_dir, img_file)
     #     img = cv2.imread(img_path)
-    #     find_cross_point(img, img_name, output_dir)
+    #     find_cross_point(img, output_dir, img_name)
